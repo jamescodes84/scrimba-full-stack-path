acfc00f4d9471c9946e4f9e332a8b094bf68a065
jamescodes84 <james.reynolds9@icloud.com>
2025-06-19 15:51:54 -0600

    sum displays at the end of a single hand

--- Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
@@ -1,13 +1,23 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="index.css">
-    </head>
-    <body>
-        <h1>Blackjack</h1>
-        <p id="message-el">Want to play a round?</p>
-        <p>Cards:</p>
-        <p>Sum:</p>
-        <button onclick="startGame()">START GAME</button>
-        <script src="index.js"></script>
-    </body>
-</html>
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+// 2. Store the sum paragraph in a variable called sumEl
+sumEl = document.getElementById("sum-el")
+function startGame() {
+    // 3. Render the sum on the page using this format -> "Sum: 14"
+    sumEl.innerText ="Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
