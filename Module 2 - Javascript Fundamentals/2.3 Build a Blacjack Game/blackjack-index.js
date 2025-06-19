30014dc2996fcf8ec74dee507205737e4a234193
jamescodes84 <james.reynolds9@icloud.com>
2025-06-19 14:51:17 -0600

    two card and sum variables created

---
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
@@ -0,0 +1,9 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = Math.random() * 10 + 1
+let secondCard = Math.random() * 10 + 1
+
+let sum = firstCard + secondCard;
