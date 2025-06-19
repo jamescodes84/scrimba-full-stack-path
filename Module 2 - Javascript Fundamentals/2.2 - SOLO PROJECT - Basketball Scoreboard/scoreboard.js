d7d31d988db6743893e956a102366e9cf636cd1b
jamescodes84 <james.reynolds9@icloud.com>
2025-06-19 13:37:42 -0600

    scoreboard functionality works

--- Module 2 - Javascript Fundamentals/2.2 - SOLO PROJECT - Basketball Scoreboard/scoreboard.js
+++ Module 2 - Javascript Fundamentals/2.2 - SOLO PROJECT - Basketball Scoreboard/scoreboard.js
@@ -1,1 +1,37 @@
-//This is placeholder so the file is loaded to git
+let homeScore = 0;
+let guestScore = 0;
+
+
+document.getElementById("home-score-div").innerText = homeScore
+document.getElementById("guest-score-div").innerText = guestScore
+
+
+function addOneHome() {
+    homeScore += 1
+    document.getElementById("home-score-div").innerText = homeScore
+}
+
+function addTwoHome() {
+    homeScore += 2
+    document.getElementById("home-score-div").innerText = homeScore
+}
+
+function addThreeHome() {
+    homeScore += 3
+    document.getElementById("home-score-div").innerText = homeScore
+}
+
+function addOneGuest() {
+    guestScore += 1
+    document.getElementById("guest-score-div").innerText = guestScore
+}
+
+function addTwoGuest() {
+    guestScore += 2
+    document.getElementById("guest-score-div").innerText = guestScore
+}
+
+function addThreeGuest() {
+    guestScore += 3
+    document.getElementById("guest-score-div").innerText = guestScore
+}
