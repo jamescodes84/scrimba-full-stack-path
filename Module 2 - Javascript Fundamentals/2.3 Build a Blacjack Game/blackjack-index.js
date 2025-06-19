f3bc91d44cb82b7cbaa7922a3cf2dd1c0c56fbfe
jamescodes84 <james.reynolds9@icloud.com>
2025-06-19 15:58:19 -0600

    card values display when you start the game

--- Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
@@ -5,11 +5,13 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-// 2. Store the sum paragraph in a variable called sumEl
-sumEl = document.getElementById("sum-el")
+let sumEl = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.querySelector("#cards-el")
 function startGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
-    sumEl.innerText ="Sum: " + sum
+    // 3. Render the cars on the page using this format -> "Cards: 10 4"
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
