1ca6538585cd8d953a99a25c5b5a9498e31ca004
jamescodes84 <james.reynolds9@icloud.com>
2025-06-19 11:55:49 -0600

    counter resets when number is saved

--- Module 2 - Javascript Fundamentals/2.1 - Build a Counter App/counter-index.js
+++ Module 2 - Javascript Fundamentals/2.1 - Build a Counter App/counter-index.js
@@ -1,20 +1,16 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let savedEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
-    // NB: Make sure to not delete the existing content of the paragraph
-    let savedCount = count + " - "
-    savedEl.innerText += savedCount
-    console.log("hit save")
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.innerText = 0
 }
-
 
