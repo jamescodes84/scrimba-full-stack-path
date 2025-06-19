efb9fb6244cf4540293b1fa146ce26a156a98cd5
jamescodes84 <james.reynolds9@icloud.com>
2025-06-19 15:46:12 -0600

    message udpates after button is clicked

--- Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
@@ -1,24 +1,13 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
-
-
-
-
-function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game! 😭"
-        isAlive = false
-    }
-}
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <h1>Blackjack</h1>
+        <p id="message-el">Want to play a round?</p>
+        <p>Cards:</p>
+        <p>Sum:</p>
+        <button onclick="startGame()">START GAME</button>
+        <script src="index.js"></script>
+    </body>
+</html>
