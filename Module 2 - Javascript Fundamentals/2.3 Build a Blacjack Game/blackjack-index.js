88ad7121c35a1b38b047c77e276df24152492506
jamescodes84 <james.reynolds9@icloud.com>
2025-06-19 15:16:56 -0600

    outpute message set to variable

--- Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
@@ -1,20 +1,20 @@
 let firstCard = 10
 let secondCard = 11
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard + 4
 let hasBlackJack = false
-// 1. Create a variable called isAlive and assign it to true
-
-// 2. Flip its value to false in the appropriate code block 
+let isAlive = true
+// 1. Declare a variable called message and assign its value to an empty string
+let message = ""
+// 2. Reassign the message variable to the string we're logging out
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-    isAlive = true
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackJack = true
-    isAlive = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
     isAlive = false
 }
 
-// 3. Log it out to check that you're doing it right
+// 3. Log it out!
+console.log(message)
