a7bf1f99d9c33e3599fe520092ab03e3cfa800f1
jamescodes84 <james.reynolds9@icloud.com>
2025-06-20 17:35:37 -0600

    code refactored for startgame to work properly

--- Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
@@ -1,29 +1,33 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-
 function getRandomCard() {
-    let randomCard = (Math.floor( Math.random()*13 ) + 1) 
-    
-    if (randomCard == 1) {
-        randomCard = 11
-    } else if ((randomCard > 10) && (randomCard < 14)){
-        randomCard = 10
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
     }
-    // if 1     -> return 11
-    // if 11-13 -> return 10
-    return randomCard
 }
 
 function startGame() {
+    isAlive = true
+    // Generate two random numbes
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    // Re-assign the cards and sum variables so that the game can start
+    cards.push(firstCard)
+    cards.push(secondCard)
+    sum = firstCard + secondCard
+    
     renderGame()
 }
 
