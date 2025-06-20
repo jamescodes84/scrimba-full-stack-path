8c98eba8c99fef2e8a9a1269904054d4dac3d47b
jamescodes84 <james.reynolds9@icloud.com>
2025-06-20 17:24:49 -0600

    new card number is generated at random

--- Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
@@ -1,4 +1,3 @@
-// 2. Use getRandomCard() to set the values of firstCard and secondCard
 let firstCard = getRandomCard()
 let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
@@ -10,12 +9,10 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-// 1. Create a function, getRandomCard(), that always returns the number 5
-function getRandomCard(){
-    
-    return 5
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+    return Math.floor(Math.random() * 13) + 1
 }
-
 
 function startGame() {
     renderGame()
@@ -42,11 +39,8 @@ function renderGame() {
 
 
 function newCard() {
-    // 3. Use the getRandomCard() to set the value of card
-    
     let card = getRandomCard()
     sum += card
     cards.push(card)
-    console.log(cards)
     renderGame()
 }
