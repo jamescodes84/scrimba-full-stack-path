93f6388f53ce3aab63999ede589a1d89ca16030a
jamescodes84 <james.reynolds9@icloud.com>
2025-06-20 18:36:53 -0600

    challenge 6 finished

---
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/BONUS: Javascript Exercises/Challenge 6 - Rock Paper Scissors.js
@@ -0,0 +1,9 @@
+let hands = ["rock", "paper", "scissor"]
+
+// Create a function that returns a random item from the array
+
+function getHang() {
+    return hands[Math.floor(Math.random() * 3)]
+}
+
+
