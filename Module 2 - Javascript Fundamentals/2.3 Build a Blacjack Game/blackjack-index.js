6351b3467bc0728ee05bf24c620359a5bfbc1a42
jamescodes84 <james.reynolds9@icloud.com>
2025-06-20 18:02:15 -0600

    player name and chips now show up on screen

--- Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/blackjack-index.js
@@ -1,3 +1,5 @@
+// 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {name: "Jame", chips: 0}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,15 +8,18 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
+// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
+let playerEl = document.getElementById("player-el")
+// 4. Render the player's name and chips in playerEl
+playerEl.innerText ="Name: " + player.name + " Chips: " + player.chips
 function getRandomCard() {
-    let randomNumer = Math.floor( Math.random()*13 ) + 1
-    if (randomNumer > 10) {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
         return 10
-    } else if (randomNumer === 1) {
+    } else if (randomNumber === 1) {
         return 11
     } else {
-        return randomNumer
+        return randomNumber
     }
 }
 
@@ -48,11 +53,10 @@ function renderGame() {
 
 
 function newCard() {
-    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
-    if (isAlive && sum < 21){
+    if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
         sum += card
         cards.push(card)
-        renderGame()
+        renderGame()        
     }
 }
