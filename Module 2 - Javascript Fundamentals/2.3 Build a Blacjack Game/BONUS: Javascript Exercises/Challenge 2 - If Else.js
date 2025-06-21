2c96758039c8de335d5e49b55061923e764ba365
jamescodes84 <james.reynolds9@icloud.com>
2025-06-20 18:28:34 -0600

    first 4 Javascript challenges added

---
+++ Module 2 - Javascript Fundamentals/2.3 Build a Blacjack Game/BONUS: Javascript Exercises/Challenge 2 - If Else.js
@@ -0,0 +1,27 @@
+let age = 15
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+
+let discount = ""
+
+if (age < 6) {
+    discount = "free"
+} else if ( age < 18 ) {
+    discount = "child discount"
+} else if ( age < 27 ) {
+    discount = "student discount"
+} else if ( age < 67 ) {
+    discount = "full price"
+} else {
+    discount = "senior citizen discount"
+}
+
+
+console.log(discount)
