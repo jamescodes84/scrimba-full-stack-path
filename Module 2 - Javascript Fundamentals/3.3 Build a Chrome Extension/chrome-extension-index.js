366318cccebaff8e3024d60a85b5467c04e7c8b5
jamescodes84 <james.reynolds9@icloud.com>
2025-06-21 10:31:53 -0600

    UL added for results

--- Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/chrome-extension-index.js
+++ Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/chrome-extension-index.js
@@ -1,13 +1,17 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
+// 2. Grab the unordered list and store it in a const variable called ulEl
+const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
-    // Push the value from the inputEl into the myLeads array 
-    // instead of the hard-coded "www.awesomeleads.com" value
-    // Google -> "get value from input field javascript"
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
+
 
 
