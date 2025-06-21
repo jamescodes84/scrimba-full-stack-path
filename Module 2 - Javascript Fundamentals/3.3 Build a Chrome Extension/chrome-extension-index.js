0b1f2a0e9a999f795812c40e296a6ba74c7949ae
jamescodes84 <james.reynolds9@icloud.com>
2025-06-21 16:26:37 -0600

    leads can be retrieved from localStorage

--- Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/chrome-extension-index.js
+++ Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/chrome-extension-index.js
@@ -3,17 +3,21 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    // Save the myLeads array to localStorage 
-    // PS: remember JSON.stringify()
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
     
     // To verify that it works:
     console.log( localStorage.getItem("myLeads") )
 })
+
+console.log(leadsFromLocalStorage)
 
 function renderLeads() {
     let listItems = ""
