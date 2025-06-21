50a77afeaaa45945facb89664602f479d1e1c8f6
jamescodes84 <james.reynolds9@icloud.com>
2025-06-21 16:19:23 -0600

    list items saved in local storage between renders

--- Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/chrome-extension-index.js
+++ Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/chrome-extension-index.js
@@ -1,4 +1,3 @@
-// chrome://extensions/
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -7,11 +6,16 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
 })
-/* */
+
 function renderLeads() {
-    console.log("rendering...")
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
