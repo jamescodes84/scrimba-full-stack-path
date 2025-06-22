9c60c89cdc0cf8c31ce3bdbf06f007b629503afc
jamescodes84 <james.reynolds9@icloud.com>
2025-06-22 14:04:51 -0600

    save tab button implemented

--- Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/chrome-extension-index.js
+++ Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/chrome-extension-index.js
@@ -4,20 +4,19 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-// 1. Grab the SAVE TAB button and store it in a tabBtn variable
-let tabBtn = document.getElementById("tab-btn")
+const tabBtn = document.getElementById("tab-btn")
+
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
-// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
-tabBtn.addEventListener("click", () => {
-    console.log(tabs[0].url)
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
 function render(leads) {
