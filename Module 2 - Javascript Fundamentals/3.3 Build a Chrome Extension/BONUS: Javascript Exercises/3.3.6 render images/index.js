7e20e09b01225b44b00570eba858868fd74df567
jamescodes84 <james.reynolds9@icloud.com>
2025-06-22 15:18:56 -0600

    js exercise 6 complete

---
+++ Module 2 - Javascript Fundamentals/3.3 Build a Chrome Extension/BONUS: Javascript Exercises/3.3.6 render images/index.js
@@ -0,0 +1,18 @@
+// Create a function that renders the three team images
+// Use a for loop, template strings (``), plus equals (+=)
+// .innerHTML to solve the challenge.
+
+const imgs = [
+    "images/hip1.jpg",
+    "images/hip2.jpg",
+    "images/hip3.jpg"
+]
+
+container = document.getElementById("container")
+let imageList = ""
+for (let i = 0; i < imgs.length; i++){
+   
+    imageList += `<img src="${imgs[i]}" />`
+}
+console.log(imageList)
+container.innerHTML = imageList
