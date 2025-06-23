e387701b3885f874d7b7633f6017705ad0fe9f1d
jamescodes84 <james.reynolds9@icloud.com>
2025-06-22 18:16:01 -0600

    all functionality implented, no styles applied

--- Module 2 - Javascript Fundamentals/2.5 SOLO PROJECT - Unit Converter/unit-converter-index.js
+++ Module 2 - Javascript Fundamentals/2.5 SOLO PROJECT - Unit Converter/unit-converter-index.js
@@ -4,7 +4,56 @@
 1 kilogram = 2.204 pound
 */
 
-convertButton = document.getElementById("convert-btn")
+const lengthOutput = document.getElementById("length-output-div")
+const volumeOutput = document.getElementById("volume-output-div")
+const massOutput = document.getElementById("mass-output-div")
+const volumeLeft = document.getElementById("volume-left")
+const volumeRight = document.getElementById("volume-right")
+const massLeft = document.getElementById("massLeft")
+const massRight = document.getElementById("mass-right")
+const numInput = document.getElementById("num-input")
+const convertButton = document.getElementById("convert-btn")
+let numberToConvert = 0
+let units = {
+    "feetToMeters" : "",
+    "metersToFeet" : "",
+    "litersToGallons" : "",
+    "gallonsToLiters" : ""
+}
 
-convertButton
 
+
+convertButton.addEventListener("click", () => {
+    numberToConvert = Number(numInput.value)
+    
+    convertUnits(numberToConvert)
+    updateView()
+})
+
+
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+function convertUnits(value){
+    units.metersToFeet = (3.281 * value).toFixed(3)
+    units.feetToMeters =(value / 3.281).toFixed(3)
+    units.litersToGallons = (0.264 * (value)).toFixed(3)
+    units.gallonsToLiters =(value / 0.264).toFixed(3)
+    units.kilogramsToPounds = (2.204 * value).toFixed(3)
+    units.poundsToKilograms = (value / 2.204).toFixed(3)
+    
+   
+}
+
+function updateView(value) {
+ 
+  lengthOutput.innerHTML = ` ${numberToConvert} meters = ${units.metersToFeet} feet | ${numberToConvert} feet = ${units.feetToMeters} meters  ` 
+  
+  volumeOutput.innerHTML = ` ${numberToConvert} liters = ${units.litersToGallons} gallons | ${numberToConvert} gallons = ${units.gallonsToLiters} liters  ` 
+  
+  massOutput.innerHTML = ` ${numberToConvert} kilos = ${units.kilogramsToPounds} pounds | ${numberToConvert} pounds = ${units.poundsToKilograms} kilos  ` 
+  
+}
