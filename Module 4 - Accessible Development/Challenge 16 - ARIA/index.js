7d0a2ebfe9ccf6275478d8500630dc6a46d3a370
jamescodes84 <james.reynolds9@icloud.com>
2025-06-24 19:25:59 -0600

    challenge 16 completed - ARIA

---
+++ Module 4 - Accessible Development/Challenge 16 - ARIA/index.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+    let body = document.body;
+    let themeButton = document.getElementById('toggleTheme');
+    
+    body.classList.toggle('dark-mode');
+    let isDarkMode = body.classList.contains('dark-mode');
+
+    themeButton.setAttribute('aria-checked', isDarkMode);
+    themeButton.textContent = isDarkMode ? 'Dark Mode' : 'Light Mode';
+    themeButton.alt = isDarkMode? 'Black button with white letters' : 'White button with dark letters'
+}
