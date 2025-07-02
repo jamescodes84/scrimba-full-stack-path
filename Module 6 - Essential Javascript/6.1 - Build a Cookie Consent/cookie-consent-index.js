b4ba7fb5d731ee14a8f7f631535c336f2d277e01
jamescodes84 <james.reynolds9@icloud.com>
2025-07-02 12:39:45 -0600

    user sees animation when form is submitted

--- Module 6 - Essential Javascript/6.1 - Build a Cookie Consent/cookie-consent-index.js
+++ Module 6 - Essential Javascript/6.1 - Build a Cookie Consent/cookie-consent-index.js
@@ -1,5 +1,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -9,19 +11,33 @@ modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
 
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
 /*
 Challenge:
-1. Take control of the form element. 
-2. Add an eventListener to the form to listen for a
-   "submit" event.
-3. When a user clicks "accept", prevent the default
-   behaviour that triggers the refresh.
-4. Log out "form submitted".
-*/  
+1. Take control of the "modal-text" element. 
+2. Make it so that when a user clicks on 
+   the accept button, the HTML string below
+   is inserted into the modal-text div.
+    
+<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>
+*/ 
+const modalText = document.getElementById('modal-text')
+modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
+`
+    
 
-const consentForm = document.getElementById('consent-form')
-
-consentForm.addEventListener("submit",(event) => {
-    event.preventDefault();
-    console.log("form submitted")
 })
+ 
+
