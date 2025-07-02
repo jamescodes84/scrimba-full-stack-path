43689ea4566fa9e58851b3b2d765fa7d5f0788d0
jamescodes84 <james.reynolds9@icloud.com>
2025-07-02 15:30:40 -0600

    decline button now swaps with accepted on hover

--- Module 6 - Essential Javascript/6.1 - Build a Cookie Consent/cookie-consent-index.js
+++ Module 6 - Essential Javascript/6.1 - Build a Cookie Consent/cookie-consent-index.js
@@ -2,30 +2,32 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
-
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceButtons = document.getElementById("modal-choice-btns")
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
-}) 
+})
+
+declineBtn.addEventListener('mouseenter', function(){
+    console.log('hovered')
+    modalChoiceButtons.classList.toggle('reverse')
+})
 
 /*   
 Challenge: 
-1. Take control of the decline btn. (What do you
-   need to do to make that possible?)
-2. Set up an eventListener that is triggered 
-   when a user's cursor hovers over the decline 
-   button. (Google the event to listen out for!)
-3. For now, just log out "hovered" when that 
-   happens. 
+1. Take control of the div holding the buttons.
+2. In index.css, set up a selector for a new class
+   that uses flex-direction to reverse the order 
+   of its child elements.
+3. Toggle that class on the div holding the buttons 
+   when a user's cursor hovers over the decline button.
 */ 
 
-const declineButton = document.getElementById('decline-btn')
-declineButton.addEventListener("mouseover", () => {
-    console.log("hovered")
-})
+
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
     
