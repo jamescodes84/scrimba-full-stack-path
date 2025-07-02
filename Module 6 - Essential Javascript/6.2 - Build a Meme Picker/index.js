5213dccaac6220f5935dfabfebeca8a14dfa623d
jamescodes84 <james.reynolds9@icloud.com>
2025-07-02 17:27:10 -0600

    looping through cats

--- Module 6 - Essential Javascript/6.2 - Build a Meme Picker/index.js
+++ Module 6 - Essential Javascript/6.2 - Build a Meme Picker/index.js
@@ -1,60 +1,185 @@
-const modal = document.getElementById('modal')
-const modalCloseBtn = document.getElementById('modal-close-btn')
-const consentForm = document.getElementById('consent-form')
-const modalText = document.getElementById('modal-text')
-const declineBtn = document.getElementById('decline-btn')
-const modalChoiceButtons = document.getElementById("modal-choice-btns")
-setTimeout(function(){
-    modal.style.display = 'inline'
-}, 1500)
+const catsData = [
+    {
+        emotionTags: ["moody"],
+        isGif: false,
+        image: "angry.jpeg",
+        alt: "A cat looking moody",
+    },
+    {
+        emotionTags: ["moody", "insomniac"],
+        isGif: false,
+        image: "angry2.jpeg",
+        alt: "A cat looking moody",
+    },
+    {
+        emotionTags: ["moody"],
+        isGif: false,
+        image: "angry3.jpeg",
+        alt: "A cat looking moody",
+    },
+    {
+        emotionTags: ["confused", "sad"],
+        isGif: false,
+        image: "confused.jpeg",
+        alt: "A cat looking confused",
+    },
+    {
+        emotionTags: ["dominant", "moody"],
+        isGif: false,
+        image: "dominant.jpeg",
+        alt: "A cat looking dominant",
+    },
+    {
+        emotionTags: ["happy", "relaxed"],
+        isGif: false,
+        image: "happy.jpeg",
+        alt: "A cat looking happy",
+    },
+    {
+        emotionTags: ["hungry"],
+        isGif: false,
+        image: "hungry.jpeg",
+        alt: "A cat looking hungry",
+    },
+    {
+        emotionTags: ["hungry"],
+        isGif: false,
+        image: "hungry1.jpeg",
+        alt: "A cat looking hungry",
+    },
+    {
+        emotionTags: ["insomniac"],
+        isGif: false,
+        image: "insomnia.jpeg",
+        alt: "A cat looking insomniac",
+    },
+    {
+        emotionTags: ["insomniac"],
+        isGif: false,
+        image: "insomnia1.jpeg",
+        alt: "A cat looking insomniac",
+    },
+    {
+        emotionTags: ["relaxed"],
+        isGif: false,
+        image: "lazy.jpeg",
+        alt: "A cat looking lazy",
+    },
+    {
+        emotionTags: ["scared"],
+        isGif: false,
+        image: "nervous.jpeg",
+        alt: "A cat looking nervous",
+    },
+    {
+        emotionTags: ["sad"],
+        isGif: false,
+        image: "sad.jpeg",
+        alt: "A cat looking sad",
+    },
+    {
+        emotionTags: ["sad", "moody"],
+        isGif: false,
+        image: "sad1.jpeg",
+        alt: "A cat looking sad",
+    },
+    {
+        emotionTags: ["moody"],
+        isGif: true,
+        image: "angry.gif",
+        alt: "A cat looking moody",
+    },
+    {
+        emotionTags: ["moody"],
+        isGif: true,
+        image: "angry2.gif",
+        alt: "A cat looking angry",
+    },
+    {
+        emotionTags: ["confused"],
+        isGif: true,
+        image: "confused2.gif",
+        alt: "A cat looking confused",
+    },
+    {
+        emotionTags: ["dominant"],
+        isGif: true,
+        image: "dominant.gif",
+        alt: "A cat looking dominant",
+    },
+    {
+        emotionTags: ["happy"],
+        isGif: true,
+        image: "happy.gif",
+        alt: "A cat looking happy",
+    },
+    {
+        emotionTags: ["hungry", "sad", "confused"],
+        isGif: true,
+        image: "confused.gif",
+        alt: "A cat looking hungry",
+    },
+    {
+        emotionTags: ["hungry"],
+        isGif: true,
+        image: "hungry.gif",
+        alt: "A cat looking hungry",
+    },
+    {
+        emotionTags: ["hungry"],
+        isGif: true,
+        image: "hungry2.gif",
+        alt: "A cat looking hungry",
+    },
+    {
+        emotionTags: ["insomniac", "scared"],
+        isGif: true,
+        image: "insomnia2.gif",
+        alt: "A cat looking insomniac",
+    },
+    {
+        emotionTags: ["relaxed"],
+        isGif: true,
+        image: "lazy.gif",
+        alt: "A cat looking relaxed",
+    },
+    {
+        emotionTags: ["relaxed"],
+        isGif: true,
+        image: "relaxed2.gif",
+        alt: "A cat looking relaxed",
+    },
+    {
+        emotionTags: ["scared", "sad"],
+        isGif: true,
+        image: "nervous.gif",
+        alt: "A cat looking nervous",
+    },
+    {
+        emotionTags: ["scared"],
+        isGif: true,
+        image: "nervous2.gif",
+        alt: "A cat looking scared",
+    },
+    {
+        emotionTags: ["sad"],
+        isGif: true,
+        image: "sad.gif",
+        alt: "A cat looking sad",
+    },
+]
 
-modalCloseBtn.addEventListener('click', function(){
-    modal.style.display = 'none'
-})
+function getEmotionsArray(cats){
+    /*
+    Challenge:
+    1. Set up a "for of" in getEmotionsArray to iterate 
+    over the data.
+    2. For now, just log out each cat object individually.
+    */
 
-declineBtn.addEventListener('mouseenter', function(){
-    console.log('hovered')
-    modalChoiceButtons.classList.toggle('reverse')
-})
+    for (let cat of cats) {
+        console.log(cat)
+    }
+    getEmotionsArray(catsData)
 
-/*   
-Challenge: 
-1. Take control of the div holding the buttons.
-2. In index.css, set up a selector for a new class
-   that uses flex-direction to reverse the order 
-   of its child elements.
-3. Toggle that class on the div holding the buttons 
-   when a user's cursor hovers over the decline button.
-*/ 
-
-
-consentForm.addEventListener('submit', function(e){
-    e.preventDefault()
-    
-    const consentFormData = new FormData(consentForm)
-    const fullName = consentFormData.get('fullName')
-    
-    modalText.innerHTML = `
-    <div class="modal-inner-loading">
-        <img src="images/loading.svg" class="loading">
-        <p id="upload-text">Uploading your data to the dark web...</p>
-    </div>` 
-    
-    setTimeout(function(){
-        document.getElementById('upload-text').innerText = `
-        Making the sale...`
-    }, 1500)
-    
-    
-    setTimeout(function(){
-        document.getElementById('modal-inner').innerHTML = `
-        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
-        <p>We just sold the rights to your eternal soul.</p>
-        <div class="idiot-gif">
-            <img src="images/pirate.gif">
-        </div>
-    `
-    modalCloseBtn.disabled = false
-    }, 3000)
-  
-}) 
+}
