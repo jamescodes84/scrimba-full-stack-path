91f1b9fa666be2cd7d8dd981a82454c9558fd5ed
jamescodes84 <james.reynolds9@icloud.com>
2025-07-03 14:01:10 -0600

    checked  button value now logged to the console

--- Module 6 - Essential Javascript/6.2 - Build a Meme Picker/index.js
+++ Module 6 - Essential Javascript/6.2 - Build a Meme Picker/index.js
@@ -1,14 +1,71 @@
-// We can much more easily target pseudo elements like :checked
-const submitBtn = document.getElementById('submit-btn')
+import { catsData } from '/data.js'
 
-submitBtn.addEventListener('click', function(){
- const checkedRadio = document.querySelector('input[type="radio"]:checked')
- console.log(checkedRadio.value)
-})
+const emotionRadios = document.getElementById('emotion-radios')
 
 /*
 Challenge:
-1. Muscle memory exercise: log out 
-   the value of the checked radio 
-   option.
+1. Set up an eventlistener which calls a new
+   function called "getMatchingCatsArray" when
+   the "Get Image" button is clicked.
+2. getMatchingCatsArray should save the value
+   of the checked radio input to a const and 
+   log out that const.
 */
+
+const getImageButton = document.getElementById('get-image-btn')
+
+getImageButton.addEventListener("click", getMatchingCatsArray)
+
+function getMatchingCatsArray() {
+    const checkedButton = document.querySelector('input[type="radio"]:checked')
+    console.log(checkedButton.value)
+}
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    // remove all instances of the highlight class
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
+
+
+function renderEmotionsRadios(cats){
+        
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
+
+
+
+
