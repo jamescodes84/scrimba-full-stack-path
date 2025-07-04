db7a424dc5a24081308b3abf5252b7e42fa022e1
jamescodes84 <james.reynolds9@icloud.com>
2025-07-04 13:45:05 -0600

    tweet data is now rendered to the page

--- Module 6 - Essential Javascript/6.3 - Build a Twitter Clone/twitter-index.js
+++ Module 6 - Essential Javascript/6.3 - Build a Twitter Clone/twitter-index.js
@@ -1,50 +1,52 @@
 import { tweetsData } from './data.js'
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
-
+const feedDiv = document.getElementById('feed')
 tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
 
 function getFeedHtml(){
     
-/*
-Challenge:
-1. Replace the for of with a forEach.
-*/
-      
     let feedHtml = ``
     
-    tweetsData.forEach((tweet)=>{
-        feedHtml += 
-            `
-                <div class="tweet">
-                    <div class="tweet-inner">
-                        <img src="${tweet.profilePic}" class="profile-pic">
-                        <div>
-                            <p class="handle">${tweet.handle}</p>
-                            <p class="tweet-text">${tweet.tweetText}</p>
-                            <div class="tweet-details">
-                                <span class="tweet-detail">
-                                    ${tweet.replies.length}
-                                </span>
-                                <span class="tweet-detail">
-                                    ${tweet.likes}
-                                </span>
-                                <span class="tweet-detail">
-                                    ${tweet.retweets}
-                                </span>
-                            </div>   
-                        </div>            
-                    </div>
-                </div>
-            `
-            return tweet
-        })
-    
-   
+    tweetsData.forEach(function(tweet){
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
+   })
    return feedHtml 
-
 }
 
-console.log(getFeedHtml())
+function render(){
+/*
+Challenge:
+1. Take control of the ‘feed’ div.
+2. Render the HTML returned by the getFeedHtml 
+   function to the 'feed' div. 
+   See if you can do this with just one line of code!
+*/
+feedDiv.innerHTML = getFeedHtml()
+}
+render()
+// call render
+
