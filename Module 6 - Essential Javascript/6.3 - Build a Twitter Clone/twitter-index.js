d8c4612ca2491b6ae7dda3d3f1358a3415ed9f5d
jamescodes84 <james.reynolds9@icloud.com>
2025-07-04 13:40:47 -0600

    data is now added to template string using foreach method

--- Module 6 - Essential Javascript/6.3 - Build a Twitter Clone/twitter-index.js
+++ Module 6 - Essential Javascript/6.3 - Build a Twitter Clone/twitter-index.js
@@ -7,68 +7,44 @@ tweetBtn.addEventListener('click', function(){
 })
 
 function getFeedHtml(){
-    /*
-    Challenge:
-    1. Use a "for of" to iterate over the data and 
-    create HTML string for each tweet using the 
-    boilerplate below. Replace UPPERCASE text
-    with data from the tweets. 
-    2. Store this HTML in a let called "feedHtml".
-    3. Log out feedHtml.
-    4. Call getFeedHtml to check it's working.
-    */  
-    let feedHtml = ""
-    for (let tweet of tweetsData) {
-       
-        feedHtml += 
-        `
-            <div class="tweet">
-                <div class="tweet-inner">
-                    <img src="${tweet.profilePic}" class="profile-pic">
-                    <div>
-                        <p class="handle">${tweet.handle}</p>
-                        <p class="tweet-text">${tweet.tweetText}</p>
-                        <div class="tweet-details">
-                            <span class="tweet-detail">
-                                ${tweet.replies.length}
-                            </span>
-                            <span class="tweet-detail">
-                                ${tweet.likes}
-                            </span>
-                            <span class="tweet-detail">
-                                ${tweet.retweets}
-                            </span>
-                        </div>   
-                    </div>            
-                </div>
-            </div>
-        `
-    }
-     console.log(feedHtml)
-}
-getFeedHtml()
-
+    
 /*
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="URL OF PROFILE PIC" class="profile-pic">
-        <div>
-            <p class="handle">TWEET HANDLE</p>
-            <p class="tweet-text">TWEET TEXT</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    NUMBER OF REPLIES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF LIKES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF RETWEETS
-                </span>
-            </div>   
-        </div>            
-    </div>
-</div>
-
+Challenge:
+1. Replace the for of with a forEach.
 */
+      
+    let feedHtml = ``
+    
+    tweetsData.forEach((tweet)=>{
+        feedHtml += 
+            `
+                <div class="tweet">
+                    <div class="tweet-inner">
+                        <img src="${tweet.profilePic}" class="profile-pic">
+                        <div>
+                            <p class="handle">${tweet.handle}</p>
+                            <p class="tweet-text">${tweet.tweetText}</p>
+                            <div class="tweet-details">
+                                <span class="tweet-detail">
+                                    ${tweet.replies.length}
+                                </span>
+                                <span class="tweet-detail">
+                                    ${tweet.likes}
+                                </span>
+                                <span class="tweet-detail">
+                                    ${tweet.retweets}
+                                </span>
+                            </div>   
+                        </div>            
+                    </div>
+                </div>
+            `
+            return tweet
+        })
+    
+   
+   return feedHtml 
 
+}
+
+console.log(getFeedHtml())
