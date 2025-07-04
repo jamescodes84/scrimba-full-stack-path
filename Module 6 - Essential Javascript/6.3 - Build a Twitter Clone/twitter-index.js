4967bbd0f094a5fe8433282509c5870a0d4ebc27
jamescodes84 <james.reynolds9@icloud.com>
2025-07-04 14:51:40 -0600

    icons added and styled to feed

--- Module 6 - Essential Javascript/6.3 - Build a Twitter Clone/twitter-index.js
+++ Module 6 - Essential Javascript/6.3 - Build a Twitter Clone/twitter-index.js
@@ -1,14 +1,28 @@
 import { tweetsData } from './data.js'
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
-const feedDiv = document.getElementById('feed')
+
 tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
 
 function getFeedHtml(){
-    
     let feedHtml = ``
+/*
+Challenge:
+1. Inside each span that has a class of "tweet-detail",
+   add an <i> tag.
+2. Give each <i> tag the classes it needs to render the
+   correct icons next to the numbers.
+   The classes you will need are:
+    fa-regular, 
+    fa-solid, 
+    fa-comment-dots, 
+    fa-heart, 
+    fa-retweet
+*/
+
+
     
     tweetsData.forEach(function(tweet){
         feedHtml += `
@@ -20,12 +34,15 @@ function getFeedHtml(){
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
+                    <i class="fa-comment-dots fa-regular"></i>
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
+                    <i class="fa-heart fa-solid"></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
+                    <i class="fa-retweet fa-solid"></i>
                     ${tweet.retweets}
                 </span>
             </div>   
@@ -38,15 +55,8 @@ function getFeedHtml(){
 }
 
 function render(){
-/*
-Challenge:
-1. Take control of the ‘feed’ div.
-2. Render the HTML returned by the getFeedHtml 
-   function to the 'feed' div. 
-   See if you can do this with just one line of code!
-*/
-feedDiv.innerHTML = getFeedHtml()
+    document.getElementById('feed').innerHTML = getFeedHtml()
 }
+
 render()
-// call render
 
