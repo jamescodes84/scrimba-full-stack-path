4119ce9175f552569c798ef16028ee1b2b952fec
jamescodes84 <james.reynolds9@icloud.com>
2025-07-07 16:28:29 -0600

    6.42 - map() method challenge 1 complete

--- Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.2 - map method/index.js
+++ Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.2 - map method/index.js
@@ -1,4 +1,49 @@
-//Convert these Miles to KM! 
-const distanceWalkedMilesArr = [140, 153, 161, 153, 128, 148]
+import { playlistArr } from '/playlist.js'
 
-const conversionFactorMilesToKm = 1.6
+/*
+Challenge
+1. Refactor the code below to use .map() 
+   instead of the for loop.
+   ⚠️ Don't worry about the commas for now.
+*/
+
+// const playlistHtml = []
+
+// for (let i = 0; i < playlistArr.length; i++) {
+//     playlistHtml.push(
+// `<section class="card">
+//     <div class="card-start">
+//         <img src="/images/${playlistArr[i].albumArt}">
+//     </div>
+//         <div class="card-mid">
+//             <h4 class="card-title">${playlistArr[i].title}</h4>
+//             <p class="card-artist">${playlistArr[i].artist}</p>
+//         </div>
+//     <div class="card-end">
+//         <p class="card-menu">...</p>
+//     </div>
+// </section>
+//     `
+//     )
+// }
+
+const playlistHtml = playlistArr.map((track)=>{
+    return (
+        `<section class="card">
+    <div class="card-start">
+        <img src="/images/${track.albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${track.title}</h4>
+            <p class="card-artist">${track.artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+    `
+    )
+})
+
+
+document.getElementById('container').innerHTML = playlistHtml
