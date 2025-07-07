5da43e5ee07a6e804b0670f2317dc22ea69f3d30
jamescodes84 <james.reynolds9@icloud.com>
2025-07-07 16:14:28 -0600

    6.4.1 object destructuring 1 complete

--- Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.1 - object destructuring/index.js
+++ Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.1 - object destructuring/index.js
@@ -1,8 +1,20 @@
-const favouriteFilm = {
-    title: "Top Gun",
-    year: "1986",
-    genre: "action",
-    star: "Tom Cruise",
-    director: "Tony Scott"
-} 
-
+const dreamHoliday = {
+    destination: 'San Francisco, California ',
+    activity: 'all day sushi by the sea',
+    accommodation: 'tiny umbrellas in our drinks',
+    companion: 'my partner in crime'
+}
+let {destination, activity, accommodation, companion} = dreamHoliday
+/* 
+Challenge
+1. Complete the object dreamHoliday with whatever
+   information is true for you. Feel free to add 
+   extra properties or change the existing ones. 
+2. Destructure the object and use the individual 
+   variables to log out one or more sentences about 
+   your dream holiday. 
+   
+E.g. "I would love to go to Austin, Texas to visit the Tesla HQ. 
+     I'd sleep in a luxury ranch and hang out with Elon Musk all day."
+*/
+console.log(`I would love to go to ${destination} to visit the city by the bay with ${companion}. I'd have ${activity} and we would need ${accommodation}`)
