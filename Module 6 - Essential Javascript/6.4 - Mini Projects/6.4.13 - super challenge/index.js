6e3a3bb2f681d4fe4f1f2e797ba87e79f690eed3
jamescodes84 <james.reynolds9@icloud.com>
2025-07-08 08:17:06 -0600

    6.4.13 - super challenge boilerplate code added

---
+++ Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.13 - super challenge/index.js
@@ -0,0 +1,41 @@
+
+/*
+SUPER CHALLENGE 💪
+
+Render out a card for each of the properties in the propertyForSaleArr array (in the 'properties' folder). Each card should have an image, a property location, a price, a comment and the TOTAL property size in square metres (each object has an array with the size in square metres of the individual rooms).
+
+If no array of properties is passed to getPropertyHtml, the placeholder property stored in placeholderPropertyObj (in the 'properties' folder) should be rendered instead.
+
+This is the JS I want you to use to complete this challenge 👇
+- import/export
+- .map()
+- .join()
+- Object destructuring
+- .reduce()
+- Default parameters
+
+The HTML and CSS have been done for you. 
+This is the HTML template 👇. Replace everything in UPPERCASE with property data.
+
+<section class="card">
+    <img src="/images/IMAGE">
+    <div class="card-right">
+        <h2>PROPERTY LOCATION</h2>
+        <h3>PRICE GBP</h3>
+        <p>COMMENT</p>
+        <h3>TOTAL SIZE IN SQUARE METRES m&sup2;</h3>
+    </div>
+</section> 
+*/
+
+function getPropertyHtml() {
+
+
+
+
+
+
+}
+
+/***** Modify 👇 by adding an argument to the function call ONLY. *****/
+document.getElementById('container').innerHTML = getPropertyHtml()
