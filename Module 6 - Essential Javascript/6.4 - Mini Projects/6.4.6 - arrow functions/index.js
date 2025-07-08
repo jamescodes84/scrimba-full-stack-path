1a0e9f5599958fcc409916aa300379108b01efd4
jamescodes84 <james.reynolds9@icloud.com>
2025-07-07 18:21:07 -0600

    arrow function challenge 2 complete

--- Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.6 - arrow functions/index.js
+++ Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.6 - arrow functions/index.js
@@ -1,12 +1,12 @@
+const speedWarning = (speedLimit, speed) => { if (speed > speedLimit) return (`You are going at ${speed} mph!`)}
 
-
-let speedWarning = (speed) => {
-   return `You are going ${speed} mph!`
-}
-console.log(speedWarning(40))
+console.log(speedWarning(30, 40))
 
 /*
 Challenge
-1. Refactor this function to use an arrow function.
-2. Make sure you write the least code possible.
+1. Refactor this function so it only warns drivers 
+   who are going over the speed limit.
+2. The function now needs to take in two parameters. 
+   The first is the speed limit, the second is the 
+   driver's actual speed.
 */
