b556b8ab5aed84d53947261a4a351382c3f5100e
jamescodes84 <james.reynolds9@icloud.com>
2025-07-11 13:24:06 -0600

    6.4.23 - debugging challenge 1 complete

--- Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.23 - debugging challenges 1 and 2/index.js
+++ Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.23 - debugging challenges 1 and 2/index.js
@@ -7,11 +7,11 @@ const minDrinkAge = 21
 const minDriveAge = 16
 
 //check button click event listener
-checkButton.value.addEventListener('click', function () {
-    let message = ''
-    const age = AgeInput.value
+checkButton.addEventListener('click', function () {
+        let message = ''
+        const age = ageInput.value
         // Age is below the minDrinkAge and minDriveAge
-        if (age > minDrinkAge && age > minDriveAge) {
+        if (age < minDrinkAge && age < minDriveAge) {
             message = `I'm sorry, you cannot drink or drive ⛔`
         // Relevant to countries where minDrinkAge is less than minDriveAge
         } else if (age >= minDrinkAge && age < minDriveAge) {
@@ -24,7 +24,8 @@ checkButton.value.addEventListener('click', function () {
             message = "You can drink 🍺 and drive 🚗 (not at the same time though!)"
         } 
         renderMessage(message)
-}
+    }
+)
 
 function renderMessage(message) {
     resultDisplay.innerText = message
