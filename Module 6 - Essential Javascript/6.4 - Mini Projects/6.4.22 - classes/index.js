c48c65d2fc8ef7609e13714bc13a932d5595f9f2
jamescodes84 <james.reynolds9@icloud.com>
2025-07-11 13:09:37 -0600

    6.4.22 - classes challenge complete

--- Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.22 - classes/index.js
+++ Module 6 - Essential Javascript/6.4 - Mini Projects/6.4.22 - classes/index.js
@@ -4,14 +4,16 @@ Rebuild this constructor function as a class.
 */
 
 
-function Character(name) {
-    this.name = name
-    this.collectedItemsArr = []
-    this.addItem = function (item) {
+class Character {
+    
+    
+    constructor (name) {
+        this.name = name
+        this.collectedItemsArr = []
+    }
+    
+    addItem (item) {
         this.collectedItemsArr.push(item)
         console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
     }
 }
-
-const wizard = new Character('Merlin')
-wizard.addItem('a wand')
