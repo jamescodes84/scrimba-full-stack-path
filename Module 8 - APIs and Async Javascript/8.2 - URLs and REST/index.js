7a959e1a0a3bb9c17d0309082f4b8d2537e16e63
jamescodes84 <james.reynolds9@icloud.com>
2025-07-19 14:20:53 -0600

    first five blogposts printed to page

--- Module 8 - APIs and Async Javascript/8.2 - URLs and REST/index.js
+++ Module 8 - APIs and Async Javascript/8.2 - URLs and REST/index.js
@@ -1,20 +1,25 @@
 /**
  Challenge:
- 
- GET a list of blog posts from the JSON Placeholder API.
- 
- BaseURL: https://apis.scrimba.com/jsonplaceholder/
- Endpoint: /posts
- 
- Since there's so many posts, let's limit the array to just 5 items.
- You can use the `.slice()` array method to just grab the first 5 objects
- from the data array that comes back from the API
- 
- Log the 5 items to the console
- */
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: 'GET'})
-    .then(response => response.json())
+ With the 5 blog post objects, display the `title` and `body`
+properties of the first 5 posts on the browser page.
+ 
+ Hints: 
+ * Create a `div` in the HTML file to store these items
+ * Loop over the items creating a string of HTML elements you 
+   can then put into the div with `innerHTML`
+ */
+let blogpostsDiv = document.getElementById('blogposts-div')
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
     .then(data => {
-        console.log(data.slice(0,5))
+        
+        console.log(postsArr)
+        for (let post of data.slice(0,5)) {
+            blogpostsDiv.innerHTML += 
+            `
+                ${post.title} , ${post.body}
+            `
+        }
+        
     })
