7a8453b430d37270b346f7751e066c7ab538a961
jamescodes84 <james.reynolds9@icloud.com>
2025-07-19 12:00:37 -0600

    first api call made to bored api, then the result is inserted into the innerHTML of a div on the page

--- Module 8 - APIs and Async Javascript/8.1 - Intro to APIs/index.js
+++ Module 8 - APIs and Async Javascript/8.1 - Intro to APIs/index.js
@@ -7,3 +7,18 @@ url: https://apis.scrimba.com/bored/api/activity
 2. Display the text of the activity in the browser
 */
 
+fetch ('https://apis.scrimba.com/bored/api/activity')
+   .then(response => response.json())
+   .then(data => {
+    
+        console.log(data)
+        
+        let activityDiv = document.getElementById('activity-div')
+        
+        activityDiv.innerHTML = `
+            <p>${data.activity}</p>
+        `
+   
+   
+   })
+   
