886f90c2ec3c8cd81e16585c5cedf975a17e28c0
jamescodes84 <james.reynolds9@icloud.com>
2025-07-21 18:50:04 -0600

    button added, card api when button is clicked, card object is logged to the console

--- Module 8 - APIs and Async Javascript/8.4 - Async JavaScript/index.js
+++ Module 8 - APIs and Async Javascript/8.4 - Async JavaScript/index.js
@@ -6,3 +6,13 @@
  Log the whole response to the console
  */
 
+const getCardButton = document.getElementById('get-card-button')
+
+
+getCardButton.addEventListener("click", () => {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/", {method:'GET'})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+})
