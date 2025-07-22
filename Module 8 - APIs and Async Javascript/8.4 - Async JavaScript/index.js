eb6212d02ffcde8048fbca1f05a8c64952b67087
jamescodes84 <james.reynolds9@icloud.com>
2025-07-21 19:07:09 -0600

    callback function moved into its own block, then its name is passed as actual argument to the timeout

--- Module 8 - APIs and Async Javascript/8.4 - Async JavaScript/index.js
+++ Module 8 - APIs and Async Javascript/8.4 - Async JavaScript/index.js
@@ -11,6 +11,10 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
  * 
  * Part 1: write a `setTimeout` command. Have it wait for 2000 ms before logging "I finally ran!" to the console
  * 
- * Part 2: Upcoming...
+ * Part 2: Move the anonymous in-line function to its own, named function
  */
-setTimeout( () => {console.log("I finally ran!")}, 2000)
+function run(){
+    console.log("I finally ran!")
+}
+
+setTimeout(run, 2000)
