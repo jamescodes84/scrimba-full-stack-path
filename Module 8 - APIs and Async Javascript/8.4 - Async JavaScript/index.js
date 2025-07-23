f552bd69b6c5e9363fa210339d354f20c97378be
jamescodes84 <james.reynolds9@icloud.com>
2025-07-22 21:16:08 -0600

    cards display properly within their borders, function is added to determine which card is winner

--- Module 8 - APIs and Async Javascript/8.4 - Async JavaScript/index.js
+++ Module 8 - APIs and Async Javascript/8.4 - Async JavaScript/index.js
@@ -1,23 +1,26 @@
 let deckId
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             deckId = data.deck_id
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
 
-document.getElementById("draw-cards").addEventListener("click", () => {
+drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards)
-            document.getElementById("cards").innerHTML = `
+            cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
+            `
+            cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
         })
@@ -25,6 +28,82 @@ document.getElementById("draw-cards").addEventListener("click", () => {
 /**
  * Challenge:
  * 
- * Create a spot in the HTML for the cards to be placed in.
- * Typical playing cards have a 5:7 ratio (width-to-height).
+ * Try to determine which of the 2 cards is the "winner" (has higher value)
+ * Aces are the card with the highest "score"
+ * 
+ * In parts:
+ * 
+ * 1. Create a function that takes 2 card objects as parameters, 
+ * `card1` and `card2`. These card objects have a property called
+ * `value`, which can be any one of the following strings, in
+ * order of rising "score":
+ * 
+ * "2", "3", "4", "5", "6", "7", "8", "9", 
+ * "10", "JACK", "QUEEN", "KING", "ACE"
+ * 
+ * I.e. "2" is the lowest score and "ACE" is the highest.
+ * 
+ * The function should determine which of the 2 cards (`card1`
+ * or `card2`) has the higher score, or if they have the same score.
+ * 
+ * Log which card wins (or "It's a tie!" 
+ * if they're the same) to the console
  */
+
+function playGame(card1, card2) {
+    let card1Score = 0
+    let card2Score = 0
+    let card
+    switch (card1.value) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "10": card1Score = parseInt(card1.value)
+                break
+        case "JACK": card1Score = 11
+                break
+        case "QUEEN": card1Score = 12
+                break
+        case "KING": card1Score = 13
+                break
+        case "ACE": card1Score = 14
+                break
+    }
+    
+    switch (card2.value) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "10": card2Score = parseInt(card2.value)
+                break
+        case "JACK": card2Score = 11
+                break
+        case "QUEEN": card2Score = 12
+                break
+        case "KING": card2Score = 13
+                break
+        case "ACE": card2Score = 14
+                break
+    }
+    
+    if (card1Score > card2Score) {
+        console.log("Card 1 wins!")
+    } else if (card1Score < card2Score) {
+        console.log("Card 2 wins!")
+    } else {
+        console.log("It's a tie!")
+    }
+    
+}
