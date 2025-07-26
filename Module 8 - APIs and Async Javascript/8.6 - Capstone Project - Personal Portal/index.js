95b49282e9574f932fdadaf4d93e7502e197d31a
jamescodes84 <james.reynolds9@icloud.com>
2025-07-26 14:27:16 -0600

    image author's name now displayed on the page

--- Module 8 - APIs and Async Javascript/8.6 - Capstone Project - Personal Portal/index.js
+++ Module 8 - APIs and Async Javascript/8.6 - Capstone Project - Personal Portal/index.js
@@ -1,26 +1,19 @@
 /**
- * Challenge: get a random image from Unsplash and set it as the background
+ * Challenge part 2: Display the image's author
  * 
- * Part 1:
+ * With the data you've already fetched, display the name of the 
+ * image author on the page. They show up as the "user" in the data
+ * returned from the API.
  * 
- * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
- * (You can change the "query" at the end to whatever theme you want)
- * 
- * Change the body's backgroundImage to: 
- * `url(<insert the URL of the iamge from the API here>)`
- * 
- * (You may need to dig around the response body a bit to find this URL)
- * 
- * (Note I've already added some CSS to resize the image within the window.
- * Instructions for this were found on CSS Tricks: 
- * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
+ * Don't worry about positioning the author in the lower-left yet.
  */
 
 async function getBackground() {
 	let res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=colorado", {method:'GET'})
 	let data = await res.json()
-	console.log(data.urls.full)
+	console.log(data)
     document.getElementById('body').style.backgroundImage = `url("${data.urls.full}")`
+    document.getElementById('image-author').textContent = data.user.name
 }
 getBackground()
 
