603c9f63a224e96cf5971b1ac33486837e8f06b1
jamescodes84 <james.reynolds9@icloud.com>
2025-07-27 13:18:52 -0600

    exceptions handled gracefully using try catch blocks

--- Module 8 - APIs and Async Javascript/8.6 - Capstone Project - Personal Portal/index.js
+++ Module 8 - APIs and Async Javascript/8.6 - Capstone Project - Personal Portal/index.js
@@ -1,32 +1,34 @@
-const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-const data = await res.json()
-document.body.style.backgroundImage = `url(${data.urls.regular})`
-document.getElementById("author").textContent = `By: ${data.user.name}`
-
-//     .catch(err => {
-//         // Use a default background image/author
-//         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-// )`
-// 		document.getElementById("author").textContent = `By: Dodi Achmad`
-//     })
-
-
-const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-if (!res.ok) {
-    throw Error("Something went wrong")
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await res.json()
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+} catch (err) {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+    document.getElementById("author").textContent = `By: Dodi Achmad`   
 }
-return res.json()
-const data = await res.json()
-document.getElementById("crypto-top").innerHTML = `
-    <img src=${data.image.small} />
-    <span>${data.name}</span>
-`
-document.getElementById("crypto").innerHTML += `
-    <p>🎯: $${data.market_data.current_price.usd}</p>
-    <p>👆: $${data.market_data.high_24h.usd}</p>
-    <p>👇: $${data.market_data.low_24h.usd}</p>
-`
-    // .catch(err => console.error(err))
+
+/**
+ * Challenge: Update the code below and in the 
+ * getCurrentLocation callback to use try...catch
+ */
+try{
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    if (!res.ok) {
+        throw Error("Something went wrong")
+    }
+    const data = await res.json()
+    document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+    `
+    document.getElementById("crypto").innerHTML += `
+    /**    <p>🎯: $${data.market_data.current_price.usd}</p>
+    /**    <p>👆: $${data.market_data.high_24h.usd}</p>
+    /**    <p>👇: $${data.market_data.low_24h.usd}</p>
+    `
+    }catch(err){console.error(err)}
 
 function getCurrentTime() {
     const date = new Date()
@@ -35,25 +37,19 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000)
 
-/**
- * Challenge: Update the callback below to use async/await
- */
-
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            document.getElementById("weather").innerHTML = `
-                <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                <p class="weather-city">${data.name}</p>
-            `
-        })
-        // .catch(err => console.error(err))
+navigator.geolocation.getCurrentPosition(async position => {
+    
+    try{
+    const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+    if (!res.ok) {
+        throw Error("Weather data not available")
+    }
+    const data = await res.json()
+    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    document.getElementById("weather").innerHTML = `
+        <img src=${iconUrl} />
+        <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+        <p class="weather-city">${data.name}</p>
+    `
+    } catch(err) { console.error(err)}
 });
