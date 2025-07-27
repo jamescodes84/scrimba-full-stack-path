61211c28e258294d044feffc77e7f754074370d9
jamescodes84 <james.reynolds9@icloud.com>
2025-07-27 13:02:58 -0600

    code refactored to use top-level awaits

--- Module 8 - APIs and Async Javascript/8.6 - Capstone Project - Personal Portal/index.js
+++ Module 8 - APIs and Async Javascript/8.6 - Capstone Project - Personal Portal/index.js
@@ -1,35 +1,39 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+const data = await res.json()
+document.body.style.backgroundImage = `url(${data.urls.regular})`
+document.getElementById("author").textContent = `By: ${data.user.name}`
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
-            <p>Current: $${data.market_data.current_price.usd}</p>
-            <p>High: $${data.market_data.high_24h.usd}</p>
-            <p>Low: $${data.market_data.low_24h.usd}</p>
-        `
-    })
-    .catch(err => console.error(err))
+//     .catch(err => {
+//         // Use a default background image/author
+//         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+// )`
+// 		document.getElementById("author").textContent = `By: Dodi Achmad`
+//     })
+
+/**
+ * Challenge: Update the fetch below to use the top-level await
+ */
+
+const res2 = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+const data2 = await res2.json()
+
+
+if (!res2.ok) {
+    throw Error("Something went wrong")
+}
+       
+   
+document.getElementById("crypto-top").innerHTML = `
+    <img src=${data2.image.small} />
+    <span>${data2.name}</span>
+`
+document.getElementById("crypto").innerHTML += `
+    <p>🎯: $${data2.market_data.current_price.usd}</p>
+    <p>👆: $${data2.market_data.high_24h.usd}</p>
+    <p>👇: $${data2.market_data.low_24h.usd}</p>
+`
+   
+    
 
 function getCurrentTime() {
     const date = new Date()
@@ -49,22 +53,10 @@ navigator.geolocation.getCurrentPosition(position => {
         .then(data => {
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
-                <div id="weather-container">
-                    <div id="weather-top">
-                        <img src=${iconUrl} />
-                        <p id="temp">${Math.round(data.main.temp)}º</p>
-                    
-                    </div>
-                        <div id="weather-bottom">
-                        <p>${data.name}</p>
-                    </div>
-                </div>
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
             `
         })
         .catch(err => console.error(err))
 });
-
-/**
- * Challenge: Try to lay out the weather similar to how
- * Momentum does it.
- */
