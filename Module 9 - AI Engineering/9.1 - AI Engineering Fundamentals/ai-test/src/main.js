ec42f1aca298fea7e33306df27ce8d8607755d16
jamescodes84 <james.reynolds9@icloud.com>
2025-07-28 20:20:44 -0600

    ai test directory moved into 9.1

--- Module 9 - AI Engineering/9.1 - AI Engineering Fundamentals/ai-test/src/main.js
+++ Module 9 - AI Engineering/9.1 - AI Engineering Fundamentals/ai-test/src/main.js
@@ -1,5 +1,5 @@
 document.getElementById("app").innerHTML = "<p>Test</p>"
-import { OPENAI_API_KEY } from '../../9.1 - AI Engineering Fundamentals/9-1apikey'
+import { OPENAI_API_KEY } from '../../9-1apikey'
 // console.log("test")
 
 // let api import.meta.env.VITE_POLYGON_API_KEY );
