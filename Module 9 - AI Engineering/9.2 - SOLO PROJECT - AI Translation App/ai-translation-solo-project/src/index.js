16fdbfe806062a7a9588868b72d41ff00633891c
jamescodes84 <james.reynolds9@icloud.com>
2025-07-29 21:31:22 -0600

    all inputs logged to console

---
+++ Module 9 - AI Engineering/9.2 - SOLO PROJECT - AI Translation App/ai-translation-solo-project/src/index.js
@@ -0,0 +1,24 @@
+const textToTranslate = document.getElementById("text-to-translate")
+const languagesForm  = document.getElementById('languages-form')
+textToTranslate.addEventListener("keydown", (event) => {
+    if (event.key ==="Enter") {
+        console.log(textToTranslate.value)
+    }
+})
+
+languagesForm.addEventListener("click", (event) => {
+
+    if (event.target.id === "translate-button") {
+        console.log("text to translate: " , textToTranslate.value)
+        event.preventDefault()
+    }
+
+    const selectedRadio = document.querySelector('input[name="language"]:checked');
+     
+    if (selectedRadio) {
+        console.log("Selected language:", selectedRadio.id);
+        // Or use selectedRadio.value if you've set a value attribute
+    } else {
+        console.log("No language selected.");
+    }
+})
