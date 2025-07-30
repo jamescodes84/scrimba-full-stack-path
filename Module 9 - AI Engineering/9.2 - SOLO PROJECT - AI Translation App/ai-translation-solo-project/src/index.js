98396ea4f8d79e7d97d7901b8b3a1b78fc25e994
jamescodes84 <james.reynolds9@icloud.com>
2025-07-29 21:51:56 -0600

    app can successfully translate user's input and it is then logged to the console

--- Module 9 - AI Engineering/9.2 - SOLO PROJECT - AI Translation App/ai-translation-solo-project/src/index.js
+++ Module 9 - AI Engineering/9.2 - SOLO PROJECT - AI Translation App/ai-translation-solo-project/src/index.js
@@ -1,7 +1,8 @@
 import OpenAI from "openai"
-import {OPENAI_API_KEY, POLYGON_API_KEY} from 'apikeys.js'
+import { OPENAI_API_KEY, POLYGON_API_KEY } from "./apikeys2"
 const textToTranslate = document.getElementById("text-to-translate")
 const languagesForm  = document.getElementById('languages-form')
+
 textToTranslate.addEventListener("keydown", (event) => {
     if (event.key ==="Enter") {
         console.log(textToTranslate.value)
@@ -9,18 +10,44 @@ textToTranslate.addEventListener("keydown", (event) => {
 })
 
 languagesForm.addEventListener("click", (event) => {
-
+    const selectedLanguage = document.querySelector('input[name="language"]:checked');
     if (event.target.id === "translate-button") {
         console.log("text to translate: " , textToTranslate.value)
         event.preventDefault()
+        translate(textToTranslate.value , selectedLanguage.value)
     }
 
-    const selectedRadio = document.querySelector('input[name="language"]:checked');
+    
      
-    if (selectedRadio) {
-        console.log("Selected language:", selectedRadio.id);
-        // Or use selectedRadio.value if you've set a value attribute
-    } else {
-        console.log("No language selected.");
-    }
+    
 })
+
+async function translate(text , language){
+     const openai = new OpenAI({
+        apiKey: OPENAI_API_KEY, 
+        dangerouslyAllowBrowser: true
+    })
+
+
+    const messages = [
+        {
+            role: 'system',
+            content: `You are an absolute expert in translating English into ${language}. You are masterful at the subtleties involved in evoking similar ideas when translating, and you keep aligned with cultural concerns with respect to dialect, syntax, etymology, etc.`
+        }, 
+        {
+            role: 'user',
+            content: `${textToTranslate.value}`
+        }
+    ]
+    console.log(messages)
+    const response = await openai.chat.completions.create( {
+        model: 'gpt-4.1',
+        messages: messages,
+        temperature: 1
+
+    })
+
+   let translation = response.choices[0].message.content
+    console.log(translation)
+    // renderReport(report)
+}
