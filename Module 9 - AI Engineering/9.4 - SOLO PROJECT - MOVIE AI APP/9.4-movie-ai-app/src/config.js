1818dbbdbed27a675563b7203f0ba8d288591d4a
jamescodes84 <james.reynolds9@icloud.com>
2025-08-02 22:52:08 -0600

    inputs are now collated in a json object and logged to the console upon clicking the lets go button

--- Module 9 - AI Engineering/9.4 - SOLO PROJECT - MOVIE AI APP/9.4-movie-ai-app/src/config.js
+++ Module 9 - AI Engineering/9.4 - SOLO PROJECT - MOVIE AI APP/9.4-movie-ai-app/src/config.js
@@ -1,16 +1,16 @@
 import OpenAI from 'openai';
 import { createClient } from "@supabase/supabase-js";
-
+import { OPENAI_API_KEY, SUPABASE_API_KEY, SUPABASE_URL } from './apikeys';
 /** OpenAI config */
-if (!process.env.OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
+if (!OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey:OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 });
 
 /** Supabase config */
-const privateKey = process.env.SUPABASE_API_KEY;
+const privateKey = SUPABASE_API_KEY;
 if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
-const url = process.env.SUPABASE_URL;
+const url = SUPABASE_URL;
 if (!url) throw new Error(`Expected env var SUPABASE_URL`);
 export const supabase = createClient(url, privateKey);
--- Module 9 - AI Engineering/9.4 - SOLO PROJECT - MOVIE AI APP/9.4-movie-ai-app/src/index.js
+++ Module 9 - AI Engineering/9.4 - SOLO PROJECT - MOVIE AI APP/9.4-movie-ai-app/src/index.js
@@ -1,2 +1,20 @@
 import { openai, supabase } from './config.js';
 
+const letsGoButton = document.getElementById('lets-go-button')
+const favoriteMovieInput = document.getElementById('favorite-movie')
+const newOrOldInput = document.getElementById('new-or-old')
+const funnyOrSeriousInput = document.getElementById('funny-or-serious')
+letsGoButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    const favMovie = favoriteMovieInput.value
+    const funnyOrSerious = funnyOrSeriousInput.value
+    const newOrOld = newOrOldInput.value
+
+    const inputJSON = 
+    {
+        favMovie: favMovie,
+        funnyOrSerious: funnyOrSerious,
+        newOrOld: newOrOld
+    }
+    console.log(inputJSON)
+})
