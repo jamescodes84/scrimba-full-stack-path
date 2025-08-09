e46f9b9c7b6e4e1105a4a8f18236a5f71a4cddc9
jamescodes84 <james.reynolds9@icloud.com>
2025-08-09 11:40:18 -0600

    server logs to console only with valid endpoint

--- Module 10 - Node.js/10.1 - Build a Node API/server.js
+++ Module 10 - Node.js/10.1 - Build a Node API/server.js
@@ -3,7 +3,9 @@ import http from 'node:http'
 const PORT = 4500
 
 const server = http.createServer((req , res) => {
-    res.end('Hello from the server')
+    if (req.url ==="/api") {
+        res.end("Access Granted")
+    }
 })
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`))
