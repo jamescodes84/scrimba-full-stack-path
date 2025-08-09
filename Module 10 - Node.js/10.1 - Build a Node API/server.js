e45522049748511b9a9ae6f38c3f65f26239ac71
jamescodes84 <james.reynolds9@icloud.com>
2025-08-09 11:40:17 -0600

    server checks url endpoint to see if it is valid

--- Module 10 - Node.js/10.1 - Build a Node API/server.js
+++ Module 10 - Node.js/10.1 - Build a Node API/server.js
@@ -1,1 +1,9 @@
 import http from 'node:http'
+
+const PORT = 4500
+
+const server = http.createServer((req , res) => {
+    res.end('Hello from the server')
+})
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))
