6e2d9b9de687b8e05799709bb721e5316b815eb3
jamescodes84 <james.reynolds9@icloud.com>
2025-08-09 11:54:04 -0600

    server now only responds if the endpoint and request method are correct

--- Module 10 - Node.js/10.1 - Build a Node API/server.js
+++ Module 10 - Node.js/10.1 - Build a Node API/server.js
@@ -4,10 +4,18 @@ const PORT = 4500
 
 const server = http.createServer((req , res) => {
     if (req.url ==="/api") {
-        res.end("Access Granted")
+        res.write("Access Granted\n")
+        switch (req.method) {
+            case 'GET':
+                res.write('You have found the cheese')
+                break
+            default:
+                res.write('no cheese for you')
+        }
     } else {
-        res.end()
+        
     }
+    res.end()
 })
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`))
