af9290fbe196292881a7d00c61fd4b1de4262410
jamescodes84 <james.reynolds9@icloud.com>
2025-08-10 07:33:24 -0600

    server now serves html to page

--- Module 10 - Node.js/10.2 - Build a Fullstack Node App/server.js
+++ Module 10 - Node.js/10.2 - Build a Fullstack Node App/server.js
@@ -1,10 +1,24 @@
 /*
 Challenge: 
-1. Initialise a nodejs project:
-	Name: “from-the-other-side”.
-    Description: “A platform for sharing ghostly encounters”.
 
-2. Enable modular js (in package.json).
+1.Set up a server that serves the string 
+    '<html><h1>The server is working</h1></html>'.
+    
+    What should the content type be? 
+    What status code should you send?
 
-hint.md for help
+2. Listen on port 8000 and log a connection message to the console.
+
+3. Open the browser to see your first served HTML.
+
 */
+
+import http from 'http'
+const PORT = 8000
+const server = http.createServer((req, res) => {
+    
+    res.setHeader("Content-type", "text/html")
+    res.write(`<html><h1>The server is working</h1></html>`)
+})
+
+server.listen(PORT , () => console.log(`server is listening on port ${PORT}`))
