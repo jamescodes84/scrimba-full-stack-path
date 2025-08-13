e8840a976a92d85eda120658cd4af51883d84d0f
jamescodes84 <james.reynolds9@icloud.com>
2025-08-12 19:00:48 -0600

    query to Select brand, model, condition, color and price from cars where the price is between $20,000 and $60,000 and the condition is between 1 and 3 and the color contains red runs locally

--- Module 11 - Databases/query.sql
+++ Module 11 - Databases/query.sql
@@ -1,7 +1,11 @@
 /*
-	Select cars made between 1980 and 1989
-		show the brand, model, year and price
+	Select brand, model, condition, color and price from cars
+		where the price is between $20,000 and $60,000
+		and the condition is between 1 and 3
+		and the color contains red
 */
-SELECT brand, model, year, price
+SELECT brand, model, condition, color, price
 FROM cars
-WHERE year BETWEEN 1980 AND 1989;
+WHERE price BETWEEN 20000 AND 60000
+AND condition BETWEEN 1 AND 3
+AND color LIKE '%red%';
