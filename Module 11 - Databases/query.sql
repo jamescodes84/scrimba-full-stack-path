719b0ebd004ee0c597382ea17044eaa901b66afa
jamescodes84 <james.reynolds9@icloud.com>
2025-08-12 18:53:21 -0600

    query to select brand model, year, and price from cars where year is between 1980 and 1989 runs locally

--- Module 11 - Databases/query.sql
+++ Module 11 - Databases/query.sql
@@ -1,11 +1,7 @@
 /*
-	Select the brand, model, year, condition and price from cars
-		where the condition is 3 or higher
-		and the year is before 1970
-		and the price is below 100,000
+	Select cars made between 1980 and 1989
+		show the brand, model, year and price
 */
-
-SELECT brand, model, year, condition, price FROM cars
-	WHERE condition >= 3
-	AND year < 1970
-	AND price < 100000;
+SELECT brand, model, year, price
+FROM cars
+WHERE year BETWEEN 1980 AND 1989;
