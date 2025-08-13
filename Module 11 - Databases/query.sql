736328fcd7bff4859aca4ded4fc49e1765f70fd9
jamescodes84 <james.reynolds9@icloud.com>
2025-08-12 18:44:18 -0600

    query to select brand, model, year, condition, and price from cars  where condition >=3 AND year < 1970 AND price < 100000 runs locally

--- Module 11 - Databases/query.sql
+++ Module 11 - Databases/query.sql
@@ -2,8 +2,10 @@
 	Select the brand, model, year, condition and price from cars
 		where the condition is 3 or higher
 		and the year is before 1970
+		and the price is below 100,000
 */
 
-SELECT brand, model, year, condition, price
-FROM cars
-WHERE condition >= 3 AND year < 1970;
+SELECT brand, model, year, condition, price FROM cars
+	WHERE condition >= 3
+	AND year < 1970
+	AND price < 100000;
