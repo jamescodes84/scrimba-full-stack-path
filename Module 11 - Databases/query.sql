dc3d96435c84a91ae11fe6df1c063c034e7b035d
jamescodes84 <james.reynolds9@icloud.com>
2025-08-13 19:24:02 -0600

    query to Select brand, model, and color from cars where the color is not red, blue, or white and the brand is none of: Aston Martin, Bentley or Jaguar and sold is false runs locally

--- Module 11 - Databases/query.sql
+++ Module 11 - Databases/query.sql
@@ -1,11 +1,11 @@
 /*
 	Select brand, model, and color from cars
-		where the color is 'red'
-		and the brand is not 'Ferrari'
-		and the car has not been sold
+		where the color is not red, blue, or white
+		and the brand is none of: Aston Martin, Bentley or Jaguar
+		and sold is false
 */
-
 SELECT brand, model, color
 FROM cars
-WHERE color = 'red' AND brand != 'Ferrari'
-AND sold is false;
+WHERE color NOT IN ('red', 'blue','white')
+AND brand NOT IN ('Aston Martin', 'Bentley', 'Jaguar')
+AND sold is FALSE; 
