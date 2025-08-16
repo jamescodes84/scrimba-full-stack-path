94703ab31de9bb72e37d043255401fd3584390a7
jamescodes84 <james.reynolds9@icloud.com>
2025-08-16 11:53:33 -0600

    query to set the condition to 1 and the price to $10,000 where the car's brand is Porsche and sold is false

--- Module 11 - Databases/query.sql
+++ Module 11 - Databases/query.sql
@@ -1,9 +1,11 @@
 /*
-	Update the record for the Aston Martin DB4 with ID 14
-		set the condition to 5
-		and the price to 465000
+	Set the condition to 1
+		and the price to $10,000
+	where the car's brand is Porsche
+		and sold is false
 */
 
-UPDATE cars set
-condition = 5, price = 465000
-WHERE id = 14;
+UPDATE cars SET 
+	condition = 1,
+	price = 10000
+WHERE brand = 'Porsche' AND sold = FALSE;
