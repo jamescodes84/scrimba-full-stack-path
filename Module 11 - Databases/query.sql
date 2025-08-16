d916b1d5f653818ed50983fceebb13f7f47ed85a
jamescodes84 <james.reynolds9@icloud.com>
2025-08-16 11:21:25 -0600

    query to Select color and count how many cars have each color find cars which have not been sold order by count in descending order only show results where the count is greater than 2 runs locally

--- Module 11 - Databases/query.sql
+++ Module 11 - Databases/query.sql
@@ -1,11 +1,14 @@
 /*
-	Select brand, model, and year from cars
-		only show the oldest 5 cars in the database
-		show cars which haven't been sold
+	Select color and count how many cars have each color
+		find cars which have not been sold
+		order by count in descending order
+		only show results where the count is greater than 2
 */
 
-SELECT brand, model, year
+SELECT color, COUNT(color)
 FROM cars
-WHERE sold != TRUE
-ORDER BY year
-LIMIT 5;
+WHERE sold = FALSE
+GROUP BY (color)
+HAVING count(color) > 2
+ORDER BY count DESC;
+
