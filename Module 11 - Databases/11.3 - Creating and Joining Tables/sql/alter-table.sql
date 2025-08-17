afd7248b39c1b76d29986863b4b44a49de9b2373
jamescodes84 <james.reynolds9@icloud.com>
2025-08-17 13:16:56 -0600

    query to insert data to backfill dealership_id column

---
+++ Module 11 - Databases/11.3 - Creating and Joining Tables/sql/alter-table.sql
@@ -0,0 +1,16 @@
+-- 1. Add the column (initially nullable)
+ALTER TABLE cars
+ADD COLUMN dealership_id INTEGER;
+
+-- 2. Insert data to backfill the dealership_id column
+
+-- Update cars, set the dealership_id to 1
+--  where the dealership_id IS NULL
+
+UPDATE  cars 
+SET dealership_id = 1
+WHERE dealership_id IS NULL;
+-- 3. Add the NOT NULL constraint
+
+
+-- 4. Add the foreign key constraint
