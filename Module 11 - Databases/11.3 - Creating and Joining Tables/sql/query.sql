654199a8d0256e0793c518e3a0f3bd8069d54027
jamescodes84 <james.reynolds9@icloud.com>
2025-08-17 14:07:20 -0600

    previous query changed to right join

--- Module 11 - Databases/11.3 - Creating and Joining Tables/sql/query.sql
+++ Module 11 - Databases/11.3 - Creating and Joining Tables/sql/query.sql
@@ -1,10 +1,9 @@
-SELECT id, brand, model FROM cars ORDER BY id;/*
+/*
 	Select name, role, city, state
 		From the staff table
-	Left join with the dealerships table where the dealership_id in staff
+	Right join with the dealerships table where the dealership_id in staff
 			matches with the id in dealerships
 */
 
-SELECT name, role, city, state
-FROM staff
-LEFT JOIN dealerships ON staff.dealership_id = dealerships.id;
+SELECT name, role, city, state FROM staff
+  RIGHT JOIN dealerships ON dealership_id = dealerships.id;
