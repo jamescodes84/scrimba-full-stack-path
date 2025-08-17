a2d19ce5edbe3f7764c3620c06ffd04fb968769e
jamescodes84 <james.reynolds9@icloud.com>
2025-08-17 13:24:02 -0600

    added null constraint to column in cars table

--- Module 11 - Databases/11.3 - Creating and Joining Tables/sql/alter-table.sql
+++ Module 11 - Databases/11.3 - Creating and Joining Tables/sql/alter-table.sql
@@ -10,7 +10,37 @@ ADD COLUMN dealership_id INTEGER;
 UPDATE  cars 
 SET dealership_id = 1
 WHERE dealership_id IS NULL;
+
 -- 3. Add the NOT NULL constraint
+ALTER TABLE cars
+ALTER COLUMN dealership_id SET NOT NULL;
 
 
 -- 4. Add the foreign key constraint
+ALTER TABLE cars
+ADD CONSTRAINT dealership_fk FOREIGN KEY (dealership_id)
+REFERENCES dealerships(id);
+
+/*
+	Alter the cars table
+		add a not null constraint to these columns:
+			brand
+			model
+			year
+			price
+			color
+			condition
+			sold
+*/
+
+ALTER TABLE cars 
+ALTER COLUMN brand SET NOT NULL,
+ALTER COLUMN model SET NOT NULL,
+ALTER COLUMN year SET NOT NULL,
+ALTER COLUMN price SET NOT NULL,
+ALTER COLUMN color SET NOT NULL,
+ALTER COLUMN condition SET NOT NULL,
+ALTER COLUMN sold SET NOT NULL;
+
+
+
