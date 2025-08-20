d5c20092672ba593a0966246e8a33c83b6b54ff4
jamescodes84 <james.reynolds9@icloud.com>
2025-08-19 21:42:55 -0600

    query to use full join to show the name, role and sold_price from staff Full join with sold_cars matching seller with staff.id

--- Module 11 - Databases/11.3 - Creating and Joining Tables/sql/query.sql
+++ Module 11 - Databases/11.3 - Creating and Joining Tables/sql/query.sql
@@ -1,9 +1,11 @@
 /*
-	Select name, role, sold_price from staff
-	Inner join with sold_cars
+	Use full join to show the name, role and sold_price
+		from staff
+	Full join with sold_cars
 		matching seller with staff.id	
 */
 
 SELECT name, role, sold_price
 FROM staff
-INNER JOIN sold_cars ON seller = staff.id;
+FULL JOIN sold_cars
+ON seller = staff.id;
