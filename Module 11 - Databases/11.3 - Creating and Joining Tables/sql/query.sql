58242dae96ddacd1936260d5908b39f8f5500ea0
jamescodes84 <james.reynolds9@icloud.com>
2025-08-22 17:42:44 -0600

    instructor's query to join multiple tables

--- Module 11 - Databases/11.3 - Creating and Joining Tables/sql/query.sql
+++ Module 11 - Databases/11.3 - Creating and Joining Tables/sql/query.sql
@@ -1,20 +1,24 @@
 /*
-	Select the name, role and city from sold_cars
-	
-	Join with the staff and dealerships tables
-		use appropriate joins to show staff who have no dealership_id
+	Show the city and state of dealerships
+		with a count of the cars sold
+		aliased as cars_sold
 		
-	Include a where clause to find
-		- null values in sold_cars
-		- staff who have the role 'Salesperson'
+	Select from sold_cars
+		join with the relevant tables
+		
+	Include dealerships which have no sold cars
+	
+	Order the count in descending order
+		
+	Hint: you may need to join using a table not included in our columns
 */
 
-SELECT 
-	S.name,
-	S.role,
-	D.city
+SELECT
+	D.city,
+	D.state,
+	COUNT(SC.id) AS cars_sold
 FROM sold_cars SC
-	FULL JOIN staff S ON SC.seller = S.id
-	LEFT JOIN dealerships D ON S.dealership_id = D.id
-WHERE SC.id IS NULL
-	AND S.role = 'Salesperson';
+	LEFT JOIN cars C ON SC.cars_id = C.id
+	RIGHT JOIN dealerships D ON C.dealership_id = D.id
+GROUP BY D.city, D.state
+ORDER BY cars_sold DESC;
