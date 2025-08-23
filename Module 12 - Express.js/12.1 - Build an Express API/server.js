40d71bf5838e92530315b6dec3006a3ed083d306
jamescodes84 <james.reynolds9@icloud.com>
2025-08-23 12:01:32 -0600

    added test data, server returns it in response body

--- Module 12 - Express.js/12.1 - Build an Express API/server.js
+++ Module 12 - Express.js/12.1 - Build an Express API/server.js
@@ -1,6 +1,21 @@
 import express from 'express'
 
+
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham"
+}
+
+
 const PORT = 8000
 const app = express()
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
+
+
+app.get('/', (req,res)=> {
+
+    res.json(celebrity)
+    // res.status(200)
+})
