7ca653d843f00b3da72259fbcf0b2b0f032675f7
jamescodes84 <james.reynolds9@icloud.com>
2025-08-23 19:22:43 -0600

    CORS code added but commented out for now

--- Module 12 - Express.js/12.1 - Build an Express API/server.js
+++ Module 12 - Express.js/12.1 - Build an Express API/server.js
@@ -5,7 +5,7 @@ import { catchError } from './controllers/catchErrors.js'
 
 const PORT = 8000
 const app = express()
-
+// app.use(cors()) // cross-origin resource sharing needed to deploy
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
 app.use('/api', apiRouter)
 app.use('', catchError)
